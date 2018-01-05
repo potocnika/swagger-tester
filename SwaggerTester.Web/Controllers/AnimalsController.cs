1e74fc584e4a46aaeae51a5d570d0fab6b509544
Ales Potocnik <ales.potocnik@informa.com>
2018-01-05 11:58:47 +0000

    Add /schema endpoint

--- SwaggerTester.Web/Controllers/AnimalsController.cs
+++ SwaggerTester.Web/Controllers/AnimalsController.cs
@@ -1,10 +1,14 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using NJsonSchema;
+using NJsonSchema.Generation;
 using NSwag.Annotations;
 using SwaggerTester.Web.Definitions;
 using SwaggerTester.Web.Models;
+using SwaggerTester.Web.Swagger;
 using System;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace SwaggerTester.Web.Controllers
 {
@@ -18,6 +22,30 @@ namespace SwaggerTester.Web.Controllers
             try
             {
                 return Ok(new SuccessEnvelope { Animals = Mock(type) });
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, new ErrorEnvelope(ex));
+            }
+        }
+
+        [SwaggerResponse(StatusCodes.Status200OK, typeof(string), Description = "All good")]
+        [SwaggerResponse(StatusCodes.Status500InternalServerError, typeof(ErrorEnvelope), Description = "Something went wrong")]
+        [HttpGet("animals/{type}/schema")]
+        public async Task<IActionResult> GetSchema([FromRoute] string type)
+        {
+            try
+            {
+                var concreteType = AnimalTypeMapper.Maps[type];
+                var settings = new JsonSchemaGeneratorSettings
+                {
+                    GenerateAbstractProperties = false,
+                    FlattenInheritanceHierarchy = true,
+                    DefaultEnumHandling = EnumHandling.String,
+                    ContractResolver = new JsonSchemaContractResolver(concreteType)
+                };
+                var schema = await JsonSchema4.FromTypeAsync(concreteType, settings);
+                return Content(schema.ToJson(), "application/json");
             }
             catch (Exception ex)
             {
